69a3954ad88f78f96e99f9062a1a27f7b315bd08
Justus <justuskasyoki57@gmail.com>
2022-12-20 11:21:43 +0300

    first commit

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializer
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_list(request):
+    if request.method == 'GET':
+        tutorials = Tutorial.objects.all()
+
+        title = request.GET.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
+
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def tutorial_detail(request, pk):
+    # find tutorial by pk (id)
+    try:
+        tutorial = Tutorial.objects.get(pk=pk)
+    except Tutorial.DoesNotExist:
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    # GET / PUT / DELETE tutorial
+
+
+@api_view(['GET'])
+def tutorial_list_published(request):
+    tutorials = Tutorial.objects.filter(published=True)
+
+    if request.method == 'GET':
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+
+
